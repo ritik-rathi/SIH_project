5f34a7a59bdeda3c18356f4795b4b426105fe98b
Ritik Rathi <rathi27ritik@gmail.com>
2019-03-03 07:25:11 +0530

    feed doneeeeee

--- lib/feed.dart
+++ lib/feed.dart
@@ -5,25 +5,22 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'dart:async';
+//import 'package:fluttertoast/fluttertoast.dart';
 
+// class Photo {
+//   final int upvote;
+//   final String disease;
+//   final String url;
 
+//   Photo({this.upvote, this.url, this.disease});
 
-
-class Photo {
-  final int upvote;
-  final String disease;
-  final String url;
-
-  Photo({this.upvote, this.url, this.disease});
-
-  factory Photo.fromJson(Map<String, dynamic> json) {
-    return Photo(
-      upvote: json['upvote'] as int,
-      disease: json['disease'] as String,
-      url: json['url'] as String,
-    );
-  }
-}
+//   factory Photo.fromJson(Map<String, dynamic> json) {
+//     return Photo(
+//       upvote: json['upvote'] as int,
+//       disease: json['disease'] as String,
+//       url: json['url'] as String,
+//     );
+//   }
 
 // class MyHomePage extends StatelessWidget {
 //   final String title;
@@ -50,71 +47,66 @@ class Photo {
 //   }
 // }
 
-
 class Feed extends StatefulWidget {
   FeedState createState() => FeedState();
 }
 
 class FeedState extends State<Feed> {
-  final List<Photo> pic;
-  FeedState({this.pic});
+  Map data;
+  List userData;
 
   final String url = 'https://floating-oasis-94041.herokuapp.com/feed';
-  List data;
 
-Future getData() async{
+  Future getData() async {
     http.Response res = await http.get(Uri.encodeFull(url));
-    debugPrint(res.body);
-}
+    data = json.decode(res.body);
+    setState(() {
+      userData = data['data'];
+      print(userData.toString());
+    });
+  }
 
-void initState(){
-  super.initState();
-  getData();
-}
+  void initState() {
+    getData().then((onValue) => {super.initState(): url});
+    //super.initState();
+    /*then((onValue) => {Toast.makeText(userData).show();});*/
+  }
 
   Widget build(BuildContext context) {
     //int _index=0;
     return Scaffold(
       body: Container(
         color: Colors.green[300],
-        // child: ListView.builder(
-        //   //itemCount: list.length,
-        //   itemBuilder: (BuildContext context, index) {
-        //     return GestureDetector(
-        //         onTap: () {},
-        //         child: Card(
-        //           margin: EdgeInsets.only(bottom: 20.0, right: 10, left: 10),
-        //           color: Colors.green[100],
-        //           child: Column(
-        //             children: <Widget>[
-        //               Container(
-        //                 margin: EdgeInsets.only(bottom: 10.0),
-        //               ),
-        //               Image(
-        //                 image: NetworkImage(getSWData()[][url]),
-        //                 width: double.infinity,
-        //                 height: 100,
-        //               ),
-        //               Container(
-        //                 padding: EdgeInsets.only(right: 10.0),
-        //               ),
-        //               Text(pic[index].disease,
-        //                   style: TextStyle(
-        //                       fontSize: 20.0,
-        //                       color: Colors.black,
-        //                       fontWeight: FontWeight.bold),
-        //                   textAlign: TextAlign.left),
-        //               Text('Upvotes: ${pic[index].upvote}',
-        //                   textAlign: TextAlign.end,
-        //                   style: TextStyle(
-        //                       color: Colors.black,
-        //                       fontSize: 20,
-        //                       fontWeight: FontWeight.bold))
-        //             ],
-        //           ),
-        //         ));
-        //   },
-        // ),
+        child: ListView.builder(
+          itemCount: userData == null ? 0 : userData.length,
+          itemBuilder: (BuildContext context, index) {
+            return Card(
+              color: Colors.yellow[200],
+              margin: EdgeInsets.only(left: 80, right: 80, bottom: 16),
+              child: Column(
+                children: <Widget>[
+                  Container(margin:EdgeInsets.only(bottom: 10.0)),
+                  Image.network(userData[index]["photoUrl"],
+                      width: 100, height: 100.0),
+                  Text(
+                    '${userData[index]["disease"]}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.black),
+                  ),
+                  Text(
+                    'Upvotes: ${userData[index]["upvote"]}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.black),
+                  )
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
