dfa7d44df6f0234e7e0f76905c85b9eb6abc9cdc
Ritik Rathi <rathi27ritik@gmail.com>
2019-03-03 11:32:45 +0530

    file crash

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -179,7 +179,8 @@ class _HomeScreenState extends State<HomeScreen> {
             )
           ],
         ),
-      ]);
+      ]))
+    );}}
 
 //   Map data;
 
@@ -227,114 +228,114 @@ class _HomeScreenState extends State<HomeScreen> {
   // }
 
   //Widget itemCard(String name, String imgPath , bool isFavourite , int voteCount){
-    return Padding(
-      padding: EdgeInsets.only(left: 15.0,right:15.0 , top: 15.0),
-      child: Container(
-        height: 150.0,
-        width: double.infinity,
-        color: Colors.white,
-        child: Row(
-          children: <Widget>[
-            Container(
-              width: 140.0,
-              height: 150.0,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(imgPath),fit: BoxFit.cover
-                )
-              ),
-            ),
-            SizedBox(width: 4.0,),
-            Column(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Text(
-                      name,
-                      style: TextStyle(
-                        fontFamily: 'Quicksand',
-                        fontSize: 17.0,
-                        fontWeight: FontWeight.bold
-                    )
-                    ),
-                    SizedBox(width: 45.0,),
-                    Material(
-                      elevation: isFavourite? 0.0 : 2.0,
-                      borderRadius: BorderRadius.circular(20.0),
-                      child: Container(
-                        height: 40.0,
-                        width: 40.0,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20.0),
-                          color: isFavourite?Colors.grey.withOpacity(0.2) : Colors.white
-                        ),
-                        child: Center(
-                          child: isFavourite? Icon(Icons.favorite , color: Colors.red,) : Icon(Icons.favorite_border)
-                        ),
-                      ),
-                    ),  
+//     return Padding(
+//       padding: EdgeInsets.only(left: 15.0,right:15.0 , top: 15.0),
+//       child: Container(
+//         height: 150.0,
+//         width: double.infinity,
+//         color: Colors.white,
+//         child: Row(
+//           children: <Widget>[
+//             Container(
+//               width: 140.0,
+//               height: 150.0,
+//               decoration: BoxDecoration(
+//                 image: DecorationImage(
+//                   image: AssetImage(imgPath),fit: BoxFit.cover
+//                 )
+//               ),
+//             ),
+//             SizedBox(width: 4.0,),
+//             Column(
+//               children: <Widget>[
+//                 Row(
+//                   children: <Widget>[
+//                     Text(
+//                       name,
+//                       style: TextStyle(
+//                         fontFamily: 'Quicksand',
+//                         fontSize: 17.0,
+//                         fontWeight: FontWeight.bold
+//                     )
+//                     ),
+//                     SizedBox(width: 45.0,),
+//                     Material(
+//                       elevation: isFavourite? 0.0 : 2.0,
+//                       borderRadius: BorderRadius.circular(20.0),
+//                       child: Container(
+//                         height: 40.0,
+//                         width: 40.0,
+//                         decoration: BoxDecoration(
+//                           borderRadius: BorderRadius.circular(20.0),
+//                           color: isFavourite?Colors.grey.withOpacity(0.2) : Colors.white
+//                         ),
+//                         child: Center(
+//                           child: isFavourite? Icon(Icons.favorite , color: Colors.red,) : Icon(Icons.favorite_border)
+//                         ),
+//                       ),
+//                     ),  
 
-                ],
+//                 ],
 
-                ),
-                 SizedBox(height: 5.0,),
-                    Container(
-                      width: 175.0,
-                      child: Text(
-                        'A common disease for tomato plants',
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                          fontFamily: 'Quicksand',
-                          color: Colors.grey,
-                          fontSize: 12.0
-                        ),
-                      ),
-                    ),
-                SizedBox(height: 10.0,),
-                Row(
-                  children: <Widget>[
-                    SizedBox(width: 35.0,),
-                    Container(
-                      height: 30.0,
-                      width: 50.0,
-                      color: Colors.white,
-                      child: Center(
-                        child:Text(
-                        '\$248',
-                        style: TextStyle(
-                          fontFamily: 'Quicksand',
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold
-                        ),
-                      )
-                      ),
-                    ),
-                    Container(
-                      height: 30.0,
-                      width: 100.0,
-                      color:Colors.white ,
-                      child: Center(
-                        child: Text(
-                          'Add to Cart',
-                          style: TextStyle(
-                            fontFamily: 'Quicksand',
-                            fontWeight:FontWeight.bold,
-                            color: Colors.white
-                          )
-                        ),
-                      ),
-                    )
-                  ],
-                )
-            ],
-            )
-          ],
-        ),
-      ),
+//                 ),
+//                  SizedBox(height: 5.0,),
+//                     Container(
+//                       width: 175.0,
+//                       child: Text(
+//                         'A common disease for tomato plants',
+//                         textAlign: TextAlign.left,
+//                         style: TextStyle(
+//                           fontFamily: 'Quicksand',
+//                           color: Colors.grey,
+//                           fontSize: 12.0
+//                         ),
+//                       ),
+//                     ),
+//                 SizedBox(height: 10.0,),
+//                 Row(
+//                   children: <Widget>[
+//                     SizedBox(width: 35.0,),
+//                     Container(
+//                       height: 30.0,
+//                       width: 50.0,
+//                       color: Colors.white,
+//                       child: Center(
+//                         child:Text(
+//                         '\$248',
+//                         style: TextStyle(
+//                           fontFamily: 'Quicksand',
+//                           color: Colors.white,
+//                           fontWeight: FontWeight.bold
+//                         ),
+//                       )
+//                       ),
+//                     ),
+//                     Container(
+//                       height: 30.0,
+//                       width: 100.0,
+//                       color:Colors.white ,
+//                       child: Center(
+//                         child: Text(
+//                           'Add to Cart',
+//                           style: TextStyle(
+//                             fontFamily: 'Quicksand',
+//                             fontWeight:FontWeight.bold,
+//                             color: Colors.white
+//                           )
+//                         ),
+//                       ),
+//                     )
+//                   ],
+//                 )
+//             ],
+//             )
+//           ],
+//         ),
+//       ),
 
-   );
-}
-        }
+//    );
+// }
+//         }
 
 
 //   @override
